0c0dba89ed95c7001c5cdb6755570a41f99dcae5
Julien <julien.ramirez@gmail.com>
2024-07-22 14:58:55 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {Typography} from "@material-tailwind/react";
+
+function App() {
+
+  return (
+    <>
+        <Typography variant={"h1"}>Coucou</Typography>
+    </>
+  )
+}
+
+export default App
